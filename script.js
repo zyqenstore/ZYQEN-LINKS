bee236b1cb1412fe334d7fe3607fa3b52016cbd4
Zyqen Store <zyqenstore@gmail.com>
2026-06-03 05:15:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -305,6 +305,29 @@ if (shareBtn) {
   });
 }
 
+// ==========================================
+// BLOQUEAR ZOOM DESKTOP
+// ==========================================
+
+document.addEventListener("wheel", (e) => {
+  if (e.ctrlKey) {
+    e.preventDefault();
+  }
+}, { passive: false });
+
+document.addEventListener("keydown", (e) => {
+  if (
+    e.ctrlKey &&
+    (
+      e.key === "+" ||
+      e.key === "-" ||
+      e.key === "=" ||
+      e.key === "0"
+    )
+  ) {
+    e.preventDefault();
+  }
+});
 
 // ==========================================
 // FINAL
