0b2b578043e5556c8c6b4361ff6b44212e53a8ad
Zyqen Store <zyqenstore@gmail.com>
2026-06-04 02:01:09 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { 
-  getAuth, 
-  GoogleAuthProvider 
+import {
+  getAuth,
+  GoogleAuthProvider
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDB3ASQ43YEhMUzw-f-gL8hSxb10Ybxhn8",
+  apiKey: "AIzaSyBQ-9n5L59yBW0FLOw5IlE7WBWGhBxodv4",
   authDomain: "zyqen-links.firebaseapp.com",
   projectId: "zyqen-links",
   storageBucket: "zyqen-links.firebasestorage.app",
@@ -17,21 +16,16 @@ const firebaseConfig = {
   appId: "1:535436335319:web:a385f91ad7e13fbc683892"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export const db = getFirestore(app);
 
-
 export const auth = getAuth(app);
-
 
 export const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
   prompt: "select_account"
 });
-
 
 export const ADMIN_EMAIL = "kaiccarvalhosouzaa@gmail.com";
